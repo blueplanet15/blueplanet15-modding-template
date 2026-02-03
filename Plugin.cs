7ba20f8e75d10cca9ac3acad1467b26cbc5cf1e7
blueplanet15 <lawhitenton62@gmail.com>
2026-02-02 21:11:53 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using UnityEngine;
+using Utilla.Attributes;
+
+namespace bp15_modding_template
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version), ModdedGamemode]
+    public class Plugin : BaseUnityPlugin
+    {
+        public bool inModded = false;
+
+        void Start()
+        {
+            // put whatever you want here
+
+            // example
+            Debug.Log($"{PluginInfo.Name} Loaded!");
+        }
+
+        void Update()
+        {
+            // put whatever you want here
+
+            // example
+            if (ControllerInputPoller.instance.rightControllerPrimaryButton)
+                Debug.Log("Controller Clicked: A");
+        }
+
+        [ModdedGamemodeJoin]
+        void OnJoin()
+        {
+            // put whatever you want here
+
+            // example
+            inModded = true;
+            Debug.Log("Joined Modded!");
+        }
+
+        [ModdedGamemodeLeave]
+        void OnLeave()
+        {
+            // put whatever you want here
+
+            // example
+            inModded = false;
+            Debug.Log("Left Modded!");
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace bp15_modding_template
+{
+    public class PluginInfo
+    {
+        public const string GUID = "com.blueplanet15.moddingtemplate";
+        public const string Name = "Blueplanet15 Modding Template";
+        public const string Version = "1.0.0";
+    }
+}
